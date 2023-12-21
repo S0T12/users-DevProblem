897a7cb1d2de5da9aaeeed8f8205ce7189dfa51a
s0a1e2 <oops2198@gmail.com>
2023-12-21 15:23:10 +0330

    fix queue name

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
       transport: Transport.RMQ,
       options: {
         urls: ['amqp://localhost:5672'],
-        queue: 'usere_queue',
+        queue: 'users_queue',
         queueOptions: {
           durable: false,
         },
