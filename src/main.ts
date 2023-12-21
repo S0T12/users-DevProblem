f5f5cbdb4b4632c0d2a4471018b85b8f89640d63
s0a1e2 <oops2198@gmail.com>
2023-12-21 15:20:38 +0330

    config RMQ

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,16 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
-    { transport: Transport.TCP },
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://localhost:5672'],
+        queue: 'usere_queue',
+        queueOptions: {
+          durable: false,
+        },
+      },
+    },
   );
   await app.listen();
 }
